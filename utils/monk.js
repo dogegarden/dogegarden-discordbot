d0d6fad23449c22dba9ab3979e8806143fff629e
plunched <smoll.pp.exe@gmail.com>
2021-05-18 20:38:57 +0200

    utils formatted

--- utils/monk.js
+++ utils/monk.js
@@ -1,35 +1,38 @@
-let monk = require('monk');
-require('dotenv').config()
+let monk = require("monk");
+require("dotenv").config();
 let db = monk(process.env.MONGO_URL);
 
 class Calls {
     static async guild(guild_id) {
         let guild_data = {
             guild_id: guild_id,
-            guild_cat: '',
-            guild_rooms: '',
-            guild_users: '',
-            guild_bots: '',
+            guild_cat: "",
+            guild_rooms: "",
+            guild_users: "",
+            guild_bots: "",
             guild_counterEnabled: false,
-            guild_prefix: '!'
-        }
-        let collection = db.get('guilds');
-        let guild = await collection.findOne({ guild_id: guild_id })
+            guild_prefix: "!",
+        };
+        let collection = db.get("guilds");
+        let guild = await collection.findOne({ guild_id: guild_id });
         if (guild === null) {
-            return (await collection.insert(guild_data))
+            return await collection.insert(guild_data);
         } else {
-            return (await guild)
+            return await guild;
         }
     }
 
     static async updateGuild(guild_id, type, value) {
-        let collection = db.get('guilds');
-        return (await collection.findOneAndUpdate({ guild_id: guild_id }, { $set: { [type]: value }}))
+        let collection = db.get("guilds");
+        return await collection.findOneAndUpdate(
+            { guild_id: guild_id },
+            { $set: { [type]: value } }
+        );
     }
 
     static async getAllGuilds() {
-        let collection = db.get('guilds');
-        return (await collection.find({}))
+        let collection = db.get("guilds");
+        return await collection.find({});
     }
 }
 
