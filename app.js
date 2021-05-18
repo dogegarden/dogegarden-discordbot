520bd55e750a0d058c7ddfea12344f1b5f3a0f9f
plunched <smoll.pp.exe@gmail.com>
2021-05-18 20:42:25 +0200

    main files formatted

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require('dotenv-flow').config();
-const Doge = require('./classes/Doge');
+require("dotenv-flow").config();
+const Doge = require("./classes/Doge");
 const client = new Doge();
 
 (async function () {
     // client.mongoose.init();
     await client.registerClient();
-    if (process.env.MODE == 'dev') {
+    if (process.env.MODE == "dev") {
         client.login(process.env.DEV_CLIENT_TOKEN);
     } else {
         client.login(process.env.CLIENT_TOKEN);
