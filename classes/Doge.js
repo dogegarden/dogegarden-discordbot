cb6a8d3065fe5ef7ab257ed8834ada25d9515f7c
plunched <smoll.pp.exe@gmail.com>
2021-05-18 20:40:17 +0200

    classes formatted

--- classes/Doge.js
+++ classes/Doge.js
@@ -1,12 +1,13 @@
-const { Client: DiscordClient, Collection } = require('discord.js');
-const path = require('path');
-const fs = require('fs').promises;
+const { Client: DiscordClient, Collection } = require("discord.js");
+const path = require("path");
+const fs = require("fs").promises;
 
 class Doge extends DiscordClient {
     constructor() {
         super({
-            partials: ['CHANNEL', 'REACTION', 'MESSAGE']
-        });        /**
+            partials: ["CHANNEL", "REACTION", "MESSAGE"],
+        });
+        /**
          * @type {Collection<string, {run: function(...args), help: { name:string, aliases: string[] }}>}
          */
         this.commands = new Collection();
@@ -16,11 +17,19 @@ class Doge extends DiscordClient {
      * @param {string} search
      */
     async resolveUser(search) {
-        if (!search || typeof search !== 'string') return null;
+        if (!search || typeof search !== "string") return null;
         let user = null;
-        if (search.match(/^<@!?(\d+)>$/)) user = await this.users.fetch(search.match(/^<@!?(\d+)>$/)[1]).catch(() => { });
-        if (search.match(/^!?(\w+)#(\d+)$/) && !user) user = this.users.cache.find((u) => u.username === search.match(/^!?(\w+)#(\d+)$/)[0] && u.discriminator === search.match(/^!?(\w+)#(\d+)$/)[1]);
-        if (!user) user = await this.users.fetch(search).catch(() => { });
+        if (search.match(/^<@!?(\d+)>$/))
+            user = await this.users
+                .fetch(search.match(/^<@!?(\d+)>$/)[1])
+                .catch(() => {});
+        if (search.match(/^!?(\w+)#(\d+)$/) && !user)
+            user = this.users.cache.find(
+                (u) =>
+                    u.username === search.match(/^!?(\w+)#(\d+)$/)[0] &&
+                    u.discriminator === search.match(/^!?(\w+)#(\d+)$/)[1]
+            );
+        if (!user) user = await this.users.fetch(search).catch(() => {});
         return user;
     }
     async registerClient() {
@@ -31,13 +40,13 @@ class Doge extends DiscordClient {
      * @private
      */
     async registerCommands() {
-        const filePath = path.join(__dirname, '..', 'commands');
+        const filePath = path.join(__dirname, "..", "commands");
         const files = await fs.readdir(filePath);
         for (const file of files) {
-            if (file.endsWith('.js')) {
+            if (file.endsWith(".js")) {
                 /**
                  * @type {{run: function(...args), help: { name:string }}}
-                */
+                 */
                 const command = require(path.join(filePath, file));
                 this.commands.set(command.help.name, command);
                 console.log(`Command - ${command.help.name}`);
@@ -48,24 +57,28 @@ class Doge extends DiscordClient {
      * @private
      */
     async registerEvents() {
-        const filePath = path.join(__dirname, '..', 'events');
+        const filePath = path.join(__dirname, "..", "events");
         const files = await fs.readdir(filePath);
         for (const file of files) {
-            if (file.endsWith('.js')) {
+            if (file.endsWith(".js")) {
                 /**
                  * @type {function(Doge, ...args)}
                  */
                 const event = require(path.join(filePath, file));
-                this.on(file.split('.')[0], (...args) => event(this, ...args));
-                console.log(`Event - ${file.split('.')[0]}`);
-                delete require.cache[require.resolve(path.join(filePath, file))];
+                this.on(file.split(".")[0], (...args) => event(this, ...args));
+                console.log(`Event - ${file.split(".")[0]}`);
+                delete require.cache[
+                    require.resolve(path.join(filePath, file))
+                ];
             }
         }
     }
 
     clean = (text) => {
-        if (typeof (text) === "string") {
-            return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+        if (typeof text === "string") {
+            return text
+                .replace(/`/g, "`" + String.fromCharCode(8203))
+                .replace(/@/g, "@" + String.fromCharCode(8203));
         } else {
             return text;
         }
